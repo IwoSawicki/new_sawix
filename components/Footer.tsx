dc82e7e18d7a96ab777931f7df99521b8290a821
Iwo Sawicki <iwo.sawicki@sawix.de>
2023-01-24 15:17:27 +0100

    Footer marquee fix - added arrow icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,26 +1,60 @@
 import { animate, easeIn } from "framer-motion";
+import Link from "next/link";
 import React from "react";
+import { BsArrowUpRight } from "react-icons/bs";
 
 export default function Footer() {
   return (
     <div>
       Footer
-      <div className="relative flex overflow-x-hidden ">
-        <div className="py-12 animate-marquee whitespace-nowrap">
-          <span className="mx-4 text-4xl">Marquee Item 1</span>
-          <span className="mx-4 text-4xl">Marquee Item 2</span>
-          <span className="mx-4 text-4xl">Marquee Item 3</span>
-          <span className="mx-4 text-4xl">Marquee Item 4</span>
-          <span className="mx-4 text-4xl">Marquee Item 5</span>
-        </div>
+      <div className="relative flex overflow-x-hidden border-y-[1px] border-white">
+        <Link href={"/kontakt"}>
+          <div className="py-6 animate-marquee whitespace-nowrap flex">
+            <button className="flex text-4xl mx-4">
+              Kontaktieren
+              <BsArrowUpRight className="ml-6" />
+            </button>
+            <button className="flex text-4xl mx-4">
+              Kontaktieren
+              <BsArrowUpRight className="ml-6" />
+            </button>
+            <button className="flex text-4xl mx-4">
+              Kontaktieren
+              <BsArrowUpRight className="ml-6" />
+            </button>
+            <button className="flex text-4xl mx-4">
+              Kontaktieren
+              <BsArrowUpRight className="ml-6" />
+            </button>
+            <button className="flex text-4xl mx-4">
+              Kontaktieren
+              <BsArrowUpRight className="ml-6" />
+            </button>
+          </div>
 
-        <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
-          <span className="mx-4 text-4xl">Marquee Item 6</span>
-          <span className="mx-4 text-4xl">Marquee Item 7</span>
-          <span className="mx-4 text-4xl">Marquee Item 8</span>
-          <span className="mx-4 text-4xl">Marquee Item 9</span>
-          <span className="mx-4 text-4xl">Marquee Item 10</span>
-        </div>
+          <div className="absolute top-0 py-6 animate-marquee2 whitespace-nowrap flex">
+            <button className="flex text-4xl mx-4">
+              Kontaktieren
+              <BsArrowUpRight className="ml-6" />
+            </button>
+            <button className="flex text-4xl mx-4">
+              Kontaktieren
+              <BsArrowUpRight className="ml-6" />
+            </button>
+            <button className="flex text-4xl mx-4">
+              Kontaktieren
+              <BsArrowUpRight className="ml-6" />
+            </button>
+            <button className="flex text-4xl mx-4">
+              Kontaktieren
+              <BsArrowUpRight className="ml-6" />
+            </button>
+            <button className="flex text-4xl mx-4">
+              Kontaktieren
+              <BsArrowUpRight className="ml-6" />
+            </button>
+          </div>
+        </Link>
       </div>
     </div>
   );
