1b6aa6899ffa6cefa4a92b2d2573ea511670ffa7
Iwo Sawicki <iwo.sawicki@sawix.de>
2023-01-24 10:54:36 +0100

    framer motion menu button animatioN

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,12 +9,6 @@ import { useEffect, useState } from "react";
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const shiftBody = () => {
-    useEffect(() => {
-      document.querySelector(".test-idk")?.classList.add("shiftUp");
-    });
-  };
-
   return (
     <div className="test-idk">
       <div className="px-6 flex text-white justify-between py-4 items-center fixed w-screen z-50">
@@ -34,34 +28,49 @@ export default function Header() {
 
       {/* ----------------------------------------------------------------------- */}
 
-      <div
+      <motion.div
+        initial={{
+          opacity: "0%",
+          translateY: "20px",
+          rotate: 180,
+          scale: 0.5,
+        }}
+        animate={{
+          opacity: "100%",
+          translateY: "0px",
+          rotate: 0,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1,
+          delay: 1,
+        }}
         onClick={() => setIsOpen(!isOpen)}
         className={`${
-          isOpen ? "gap-0" : "gap-[10px]"
-        } flex flex-col items-center justify-center z-50 h-16 w-16 rounded-full bg-[#E8E5E3] fixed bottom-3 inset-x-0 mx-auto`}
+          isOpen ? "gap-0 bg-black" : "gap-[10px] bg-blue-700 "
+        } flex flex-col items-center justify-center z-50 h-16 w-16 rounded-full fixed bottom-3 inset-x-0 mx-auto`}
       >
-        <div
+        <motion.div
           className={`${
             isOpen ? "-rotate-45" : " "
-          } bg-black h-[2px] w-8 rounded-full duration-100`}
-        ></div>
-        <div
+          } bg-white h-[2px] w-8 rounded-full duration-100`}
+        ></motion.div>
+        <motion.div
           className={`${
             isOpen ? "rotate-45" : ""
-          } bg-black h-[2px] w-8 rounded-full duration-100`}
-        ></div>
-      </div>
+          } bg-white h-[2px] w-8 rounded-full duration-100`}
+        ></motion.div>
+      </motion.div>
 
       {/* ----------------------------------------------------------------------- */}
 
-      <div
+      <motion.div
         className={`${
           isOpen ? "block z-40 opacity-100" : "block -z-40 opacity-0 h-0 w-0"
         } fixed inset-x-0 mx-auto bottom-3 h-[50vh] w-[95%] bg-[#E8E5E3] rounded-xl text-black transition-all duration-300 p-4`}
       >
         <div className="">Menu coming soon</div>
-        <button onClick={shiftBody}>Shift Up!!</button>
-      </div>
+      </motion.div>
     </div>
   );
 }
