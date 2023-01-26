271e00397c42a024335f817ffa034b882572a171
Iwo Sawicki <iwo.sawicki@sawix.de>
2023-01-26 19:21:49 +0100

    moving button in jobs

--- components/Jobs.tsx
+++ components/Jobs.tsx
@@ -18,9 +18,25 @@ export default function Jobs() {
           vergeben haben und bewirb dich gerne jederzeit.
         </p>
         <Link href={"/jobs"}>
-          <button className="bg-transparent border-white border-[1px] rounded-full pl-8 mb-2 mr-2 flex justify-between items-center hover:bg-blue-700 hover:border-blue-700 duration-300 group">
-            Zu den Jobs
-            <span className="bg-blue-700 rounded-full p-3 m-[1px] ml-6 group-hover:bg-white duration-300">
+          <button className="bg-transparent border-white border-[1px] rounded-full mb-2 mr-2 flex justify-between items-center hover:bg-blue-700 hover:border-blue-700 duration-300 group">
+            <div className="group relative flex overflow-x-hidden duration-300 max-w-[200px]">
+              <Link className="FooterMarqueeLink" href={"/kontakt"}>
+                <div className=" animate-marquee whitespace-nowrap flex">
+                  <button className="flex  mx-4">Zu den Jobs</button>
+                  <button className="flex  mx-4">Zu den Jobs</button>
+                  <button className="flex  mx-4">Zu den Jobs</button>
+                  <button className="flex  mx-4">Zu den Jobs</button>
+                </div>
+
+                <div className="absolute top-0 animate-marquee2 whitespace-nowrap flex">
+                  <button className="flex  mx-4">Zu den Jobs</button>
+                  <button className="flex  mx-4">Zu den Jobs</button>
+                  <button className="flex  mx-4">Zu den Jobs</button>
+                  <button className="flex  mx-4">Zu den Jobs</button>
+                </div>
+              </Link>
+            </div>
+            <span className="bg-blue-700 rounded-full p-3 m-[1px] ml-0 group-hover:bg-white duration-300">
               <BsArrowUpRight className=" text-white rounded-full group-hover:text-black duration-300 group-hover:rotate-45" />
             </span>
           </button>
