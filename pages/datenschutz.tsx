2eb14da5f343eca07c39f1c23c364c9117479ad0
Iwo Sawicki <iwo.sawicki@sawix.de>
2023-02-09 20:59:48 +0100

    datenschutz & impressum added

---
+++ pages/datenschutz.tsx
@@ -0,0 +1,24 @@
+import Header from "@/components/Header";
+import Head from "next/head";
+
+export default function Datenschutz() {
+  return (
+    <>
+      <div>
+        <Head>
+          <title>Datenschutzerklärung - Sawix Studio</title>
+          <meta
+            name="description"
+            content="Schau dir hier unsere Datenschutzerklärung an. Mit der Benutzung der Webseite erklärst du dich mit der Datenschutzerklärung einverstanden."
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.svg" />
+        </Head>
+        <div>
+          <Header />
+          <main></main>
+        </div>
+      </div>
+    </>
+  );
+}
