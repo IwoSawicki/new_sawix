378782baed9fd4fdcfefc7738444e1ed2572b6d6
Iwo Sawicki <iwo.sawicki@sawix.de>
2023-03-17 20:32:56 +0100

    Portfolio Site Added, link to subpage done

---
+++ pages/projekte/mediceo.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function Mediceo() {
+  return (
+    <>
+      <Head>
+        <title>Mediceo Fallstudie - Sawix Studio</title>
+        <meta
+          name="description"
+          content="Sieht dir unsere Mediceo Fallstudie an und erfahre mehr zu unserem"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <div>
+        <h1>Mediceo</h1>
+      </div>
+    </>
+  );
+}
