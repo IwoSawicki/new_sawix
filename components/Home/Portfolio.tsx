93349a97f7912bfa2f8d1f9f3344aeaddafd47b6
Iwo Sawicki <iwo.sawicki@sawix.de>
2023-03-19 17:49:58 +0100

    space between image added

--- components/Home/Portfolio.tsx
+++ components/Home/Portfolio.tsx
@@ -19,44 +19,50 @@ export default function () {
         {/* ----------------------------- Row 1  -----------------------------*/}
         <div className="flex gap-12 flex-col md:flex-row">
           {/* <Link href={"/projekte/mediceo"} className="w-full md:w-1/2"> */}
-          <div className=" bg-[#FFEE62] px-4 pt-4 rounded-2xl relative w-full group hover:scale-[100.5%] hover:shadow-lg hover:shadow-white/5 duration-300">
-            <h3 className="text-3xl mb-3">Mediceo </h3>
-            <div className="mb-4 flex flex-wrap">
-              <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
-                Design & Entwicklung
-              </button>
-              {/* <button className="bg-black border-black border-[1px] rounded-full mb-2 mr-2 group-hover:rotate-45 duration-300">
+          <div className=" bg-[#FFEE62] px-4 pt-4 rounded-2xl relative w-full group hover:scale-[100.5%] hover:shadow-lg hover:shadow-white/5 duration-300 flex flex-col justify-between">
+            <div>
+              <h3 className="text-3xl mb-3">Mediceo </h3>
+              <div className="mb-4 flex flex-wrap">
+                <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
+                  Design & Entwicklung
+                </button>
+                {/* <button className="bg-black border-black border-[1px] rounded-full mb-2 mr-2 group-hover:rotate-45 duration-300">
                   <div className="h-6 w-6 md:h-7 md:w-7 flex items-center justify-center">
                   <BsArrowUpRight className="text-white" />
                   </div>
                 </button> */}
+              </div>
             </div>
-            <Image
-              src={PortfolioMediceo}
-              alt="Medi.ceo Portfolio Vorstellung"
-              className="rounded-t-2xl object-bottom"
-            />
+            <div>
+              <Image
+                src={PortfolioMediceo}
+                alt="Medi.ceo Portfolio Vorstellung"
+                className="rounded-t-2xl object-bottom"
+              />
+            </div>
           </div>
           {/* </Link> */}
           {/* ----------------------------- ----------------------------- */}
           {/* <Link href={"/portfolio/dmk-haus"} className="w-full md:w-1/2"> */}
-          <div className=" bg-[#6EE863] px-4 pt-4 rounded-2xl w-full group hover:scale-[100.5%] hover:shadow-lg hover:shadow-white/5 duration-300">
-            <h3 className="text-3xl mb-3">DMK-Haus </h3>
-            <div className="mb-4 flex flex-wrap">
-              <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
-                Design & Entwicklung
-              </button>
-              <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
-                Online-Shop
-              </button>
-              <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
-                Branding
-              </button>
-              {/* <button className="bg-black border-black border-[1px] rounded-full mb-2 mr-2 group-hover:rotate-45 duration-300">
+          <div className=" bg-[#6EE863] px-4 pt-4 rounded-2xl w-full group hover:scale-[100.5%] hover:shadow-lg hover:shadow-white/5 duration-300 flex flex-col justify-between">
+            <div>
+              <h3 className="text-3xl mb-3">DMK-Haus </h3>
+              <div className="mb-4 flex flex-wrap">
+                <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
+                  Design & Entwicklung
+                </button>
+                <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
+                  Custom-Shop
+                </button>
+                <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
+                  Branding
+                </button>
+                {/* <button className="bg-black border-black border-[1px] rounded-full mb-2 mr-2 group-hover:rotate-45 duration-300">
                   <div className="h-6 w-6 md:h-7 md:w-7 flex items-center justify-center">
-                    <BsArrowUpRight className="text-white" />
+                  <BsArrowUpRight className="text-white" />
                   </div>
                 </button> */}
+              </div>
             </div>
             <Image
               src={PortfolioDMK}
@@ -70,21 +76,22 @@ export default function () {
         {/* ----------------------------- Row 2 -----------------------------*/}
         <div className="flex gap-12 flex-col md:flex-row">
           {/* <Link href={"/portfolio/weinhaus"} className="w-full md:w-1/2"> */}
-          <div className=" bg-[#9747FF] px-4 pt-4 rounded-2xl w-full group hover:scale-[100.5%] hover:shadow-lg hover:shadow-white/5 duration-300">
-            {/* bg-[#9747FF] */}
-            <h3 className="text-3xl mb-3">Wohlfühlerei Weinhaus</h3>
-            <div className="mb-4 flex flex-wrap">
-              <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
-                Design & Entwicklung
-              </button>
-              <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
-                Online-Shop
-              </button>
-              {/* <button className="bg-black border-black border-[1px] rounded-full mb-2 mr-2 group-hover:rotate-45 duration-300">
+          <div className=" bg-[#9747FF] px-4 pt-4 rounded-2xl w-full group hover:scale-[100.5%] hover:shadow-lg hover:shadow-white/5 duration-300 flex flex-col justify-between">
+            <div>
+              <h3 className="text-3xl mb-3">Wohlfühlerei Weinhaus</h3>
+              <div className="mb-4 flex flex-wrap">
+                <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
+                  Design & Entwicklung
+                </button>
+                <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
+                  Online-Shop
+                </button>
+                {/* <button className="bg-black border-black border-[1px] rounded-full mb-2 mr-2 group-hover:rotate-45 duration-300">
                   <div className="h-6 w-6 md:h-7 md:w-7 flex items-center justify-center">
-                    <BsArrowUpRight className="text-white" />
+                  <BsArrowUpRight className="text-white" />
                   </div>
                 </button> */}
+              </div>
             </div>
             <Image
               src={PortfolioWeinhaus}
@@ -95,17 +102,19 @@ export default function () {
           {/* </Link> */}
           {/* ----------------------------- ----------------------------- */}
           {/* <Link href={"/portfolio/cdu-muehltal"} className="w-full md:w-1/2"> */}
-          <div className=" bg-[#FF8540] px-4 pt-4 rounded-2xl w-full group hover:scale-[100.5%] hover:shadow-lg hover:shadow-white/5 duration-300">
-            <h3 className="text-3xl mb-3">CDU Mühltal</h3>
-            <div className="mb-4 flex flex-wrap">
-              <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
-                Design & Entwicklung
-              </button>
-              {/* <button className="bg-black border-black border-[1px] rounded-full mb-2 mr-2 group-hover:rotate-45 duration-300">
+          <div className=" bg-[#FF8540] px-4 pt-4 rounded-2xl w-full group hover:scale-[100.5%] hover:shadow-lg hover:shadow-white/5 duration-300 flex flex-col justify-between">
+            <div>
+              <h3 className="text-3xl mb-3">CDU Mühltal</h3>
+              <div className="mb-4 flex flex-wrap">
+                <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
+                  Design & Entwicklung
+                </button>
+                {/* <button className="bg-black border-black border-[1px] rounded-full mb-2 mr-2 group-hover:rotate-45 duration-300">
                   <div className="h-6 w-6 md:h-7 md:w-7 flex items-center justify-center">
                     <BsArrowUpRight className="text-white" />
-                  </div>
-                </button> */}
+                    </div>
+                  </button> */}
+              </div>
             </div>
             <Image
               src={PortfolioCDU}
@@ -122,20 +131,22 @@ export default function () {
             href={"/portfolio/freidenker-campus"}
             className="w-full md:w-1/2"
           > */}
-          <div className=" bg-[#62B2FB] px-4 pt-4 rounded-2xl relative block w-full group hover:scale-[100.5%] hover:shadow-lg hover:shadow-white/5 duration-300">
-            <h3 className="text-3xl mb-3">Freidenker-Campus </h3>
-            <div className="mb-4 flex flex-wrap">
-              <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
-                Entwicklung
-              </button>
-              <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
-                Online-Shop
-              </button>
-              {/* <button className="bg-black  border-black border-[1px] rounded-full mb-2 mr-2 group-hover:rotate-45 duration-300">
+          <div className=" bg-[#62B2FB] px-4 pt-4 rounded-2xl relative block w-full group hover:scale-[100.5%] hover:shadow-lg hover:shadow-white/5 duration-300 flex flex-col justify-between">
+            <div>
+              <h3 className="text-3xl mb-3">Freidenker-Campus </h3>
+              <div className="mb-4 flex flex-wrap">
+                <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
+                  Entwicklung
+                </button>
+                <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
+                  Online-Shop
+                </button>
+                {/* <button className="bg-black  border-black border-[1px] rounded-full mb-2 mr-2 group-hover:rotate-45 duration-300">
                   <div className="h-6 w-6 md:h-7 md:w-7 flex items-center justify-center">
-                    <BsArrowUpRight className="text-white" />
+                  <BsArrowUpRight className="text-white" />
                   </div>
                 </button> */}
+              </div>
             </div>
             <Image
               src={PortfolioFreidenker}
@@ -146,20 +157,22 @@ export default function () {
           {/* </Link> */}
           {/* ----------------------------- ----------------------------- */}
           {/* <Link href={"/portfolio/pilipp"} className="w-full md:w-1/2"> */}
-          <div className=" bg-[#FB4B4B] px-4 pt-4 rounded-2xl w-full group hover:scale-[100.5%] hover:shadow-lg hover:shadow-white/5 duration-300">
-            <h3 className="text-3xl mb-3">Pilipp-Holzwerkstoffe</h3>
-            <div className="mb-4 flex flex-wrap">
-              <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
-                Entwicklung
-              </button>
-              <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
-                Online-Shop
-              </button>
-              {/* <button className="bg-black border-black border-[1px] rounded-full mb-2 mr-2 group-hover:rotate-45 duration-300">
+          <div className=" bg-[#FB4B4B] px-4 pt-4 rounded-2xl w-full group hover:scale-[100.5%] hover:shadow-lg hover:shadow-white/5 duration-300 flex flex-col justify-between">
+            <div>
+              <h3 className="text-3xl mb-3">Pilipp-Holzwerkstoffe</h3>
+              <div className="mb-4 flex flex-wrap">
+                <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
+                  Entwicklung
+                </button>
+                <button className=" bg-transparent border-black border-[1px] rounded-full px-3 py-1 mb-2 mr-2 text-xs md:text-sm font-light">
+                  Custom-Shop
+                </button>
+                {/* <button className="bg-black border-black border-[1px] rounded-full mb-2 mr-2 group-hover:rotate-45 duration-300">
                   <div className="h-6 w-6 md:h-7 md:w-7 flex items-center justify-center">
-                    <BsArrowUpRight className="text-white" />
+                  <BsArrowUpRight className="text-white" />
                   </div>
                 </button> */}
+              </div>
             </div>
 
             <Image
