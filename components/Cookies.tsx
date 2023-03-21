c3b113e2f023164fae999adc0ef94a2bf371e38b
Iwo Sawicki <iwo.sawicki@sawix.de>
2023-03-21 19:37:08 +0100

    portfolio git sync

---
+++ components/Cookies.tsx
@@ -0,0 +1,9 @@
+export default function Cookies() {
+  return (
+    <>
+      <div className="w-full bg-black h-9 fixed bottom-0 px">
+        <div>Hello World</div>
+      </div>
+    </>
+  );
+}
