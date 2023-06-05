d73f41b899aa530d15820586ced68b745cc55872
Iwo Sawicki <iwo.sawicki@sawix.de>
2023-06-05 19:25:18 +0200

    fertige portfoliokarten auf der projektpage DMK Haus MEDICEO WEINHAUS

--- pages/projekte/index.tsx
+++ pages/projekte/index.tsx
@@ -4,6 +4,8 @@ import Head from "next/head";
 import Image from "next/image";
 import Portfolio from "@/components/Home/Portfolio";
 
+import Link from "next/link";
+
 import { BsArrowUpRight } from "react-icons/bs";
 
 import portolioDmkhaus from "@/public/portfolio/portfolio-dmkhaus.png";
@@ -46,81 +48,84 @@ export default function portfolio() {
           <div className="text-black pt-8 flex gap-12 flex-col">
             {/* DMK HAUS */}
             <div>
-              <div className="bg-[#87AA85] rounded-xl lg:w-4/5">
-                <div className="px-4 pt-4 md:px-6 md:pt-6">
-                  <p className="text-md md:text-xl font-light">DMK-Haus</p>
-                  <h3 className="font-medium text-2xl inline pr-2 md:text-5xl">
-                    Custom Shop für Modulhäußer und Möbel rund ums Haus
-                  </h3>
+              <div className="bg-[#87AA85] rounded-xl lg:w-4/5 group hover:scale-[100.5%] duration-300">
+                <Link href={"/projekte/dmkhaus/"}>
+                  <div className="px-4 pt-4 md:px-7 md:pt-6">
+                    <p className="text-md md:text-xl font-light">DMK-Haus</p>
+                    <h3 className="font-medium text-2xl md:text-5xl relative">
+                      Custom Shop für Modulhäußer und Möbel rund ums Haus
+                      <button className="bg-black border-black border-[1px] rounded-full mx-2 group-hover:rotate-45 duration-300 md:absolute md:mt-2">
+                        <div className="h-5 w-5 md:h-8 md:w-8 flex items-center justify-center">
+                          <BsArrowUpRight className="text-white text-sm" />
+                        </div>
+                      </button>
+                    </h3>
+                  </div>
 
-                  <button className="bg-black border-black border-[1px] rounded-full mb-2 mr-2 group-hover:rotate-45 duration-300 inline">
-                    <div className="h-5 w-5 md:h-8 md:w-8 flex items-center justify-center">
-                      <BsArrowUpRight className="text-white text-sm" />
-                    </div>
-                  </button>
-                </div>
-
-                <div className="pt-6 md:flex md:justify-center">
-                  <Image
-                    src={portolioDmkhaus}
-                    alt="DMK Haus Mockup portfolio"
-                    className=""
-                  />
-                </div>
+                  <div className="pt-6 md:flex md:justify-center">
+                    <Image
+                      src={portolioDmkhaus}
+                      alt="DMK Haus Mockup portfolio"
+                      className=""
+                    />
+                  </div>
+                </Link>
               </div>
             </div>
             {/* ENDE DMK HAUS */}
             {/* MEDICEO */}
             <div className="lg:flex lg:justify-end">
-              <div className="bg-[#FFF385] rounded-xl lg:w-4/5">
-                <div className="px-4 pt-4 md:px-6 md:pt-6">
-                  <p className="text-md md:text-xl font-light">Mediceo</p>
-                  <h3 className="font-medium text-2xl inline pr-2 md:text-5xl">
-                    Website redesign für eine innovative Medizinapp
-                  </h3>
+              <div className="bg-[#FFF385] rounded-xl lg:w-4/5 group hover:scale-[100.5%] duration-300">
+                <Link href={"/projekte/dmkhaus/"}>
+                  <div className="px-4 pt-4 md:px-7 md:pt-6">
+                    <p className="text-md md:text-xl font-light">MEDICEO</p>
+                    <h3 className="font-medium text-2xl md:text-5xl relative">
+                      Website redesign für eine innovative Medizinapp
+                      <button className="bg-black border-black border-[1px] rounded-full mx-2 group-hover:rotate-45 duration-300 md:absolute md:mt-2">
+                        <div className="h-5 w-5 md:h-8 md:w-8 flex items-center justify-center">
+                          <BsArrowUpRight className="text-white text-sm" />
+                        </div>
+                      </button>
+                    </h3>
+                  </div>
 
-                  <button className="bg-black border-black border-[1px] rounded-full mb-2 mr-2 group-hover:rotate-45 duration-300 inline">
-                    <div className="h-5 w-5 md:h-7 md:w-7 flex items-center justify-center">
-                      <BsArrowUpRight className="text-white text-sm" />
-                    </div>
-                  </button>
-                </div>
-
-                <div className="pt-6 md:flex md:justify-center">
-                  <Image
-                    src={portolioDmkhaus}
-                    alt="DMK Haus Mockup portfolio"
-                    className=""
-                  />
-                </div>
+                  <div className="pt-6 md:flex md:justify-center">
+                    <Image
+                      src={portolioDmkhaus}
+                      alt="DMK Haus Mockup portfolio"
+                      className=""
+                    />
+                  </div>
+                </Link>
               </div>
             </div>
             {/* ENDE MEDICEO */}
             {/* WEINHAUS */}
             <div>
-              <div className="bg-[#A282FF] rounded-xl lg:w-4/5">
-                <div className="px-4 pt-4 md:px-6 md:pt-6">
-                  <p className="text-md md:text-xl font-light">
-                    Wohlfühlerei Weinhaus
-                  </p>
-                  <h3 className="font-medium text-2xl inline pr-2 md:text-5xl">
-                    Elegante Webseite für einen lokalen Weinanbau
-                  </h3>
+              <div className="bg-[#A282FF] rounded-xl lg:w-4/5 group hover:scale-[100.5%] duration-300">
+                <Link href={"/projekte/dmkhaus/"}>
+                  <div className="px-4 pt-4 md:px-7 md:pt-6">
+                    <p className="text-md md:text-xl font-light">
+                      Wohlfühlerei Weinhaus
+                    </p>
+                    <h3 className="font-medium text-2xl md:text-5xl relative">
+                      Elegante Webseite für einen lokalen Weinanbau
+                      <button className="bg-black border-black border-[1px] rounded-full mx-2 group-hover:rotate-45 duration-300 md:absolute md:mt-2">
+                        <div className="h-5 w-5 md:h-8 md:w-8 flex items-center justify-center">
+                          <BsArrowUpRight className="text-white text-sm" />
+                        </div>
+                      </button>
+                    </h3>
+                  </div>
 
-                  <button className="bg-black border-black border-[1px] rounded-full mb-2 mr-2 group-hover:rotate-45 duration-300 inline">
-                    <div className="h-5 w-5 md:h-7 md:w-7 flex items-center justify-center">
-                      <BsArrowUpRight className="text-white text-sm" />
-                    </div>
-                  </button>
-                </div>
-
-                <div className="pt-6 md:flex md:justify-center">
-                  <Image
-                    src={portolioDmkhaus}
-                    alt="DMK Haus Mockup portfolio"
-                    className=""
-                  />
-                </div>
+                  <div className="pt-6 md:flex md:justify-center">
+                    <Image
+                      src={portolioDmkhaus}
+                      alt="DMK Haus Mockup portfolio"
+                      className=""
+                    />
+                  </div>
+                </Link>
               </div>
             </div>
             {/* ENDE WEINHAUS */}
